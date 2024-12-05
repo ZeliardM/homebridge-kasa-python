ca28441b5c1156794b89c95f5b6e295e167a67c8
ZeliardM <ZeliardM@gmail.com>
2024-12-05 16:33:39 -0500

    Correct logging from merge

--- src/python/kasaApi.py
+++ src/python/kasaApi.py
@@ -117,7 +117,7 @@ async def discover_devices(username=None, password=None, additional_broadcasts=N
     async def on_discovered(device: Device):
         try:
             await device.update()
-            app.logger.debug(f"Discovered device: {device.host}")
+            app.logger.debug(f"Discovered device  has been updated: {device.host}")
         except UnsupportedDeviceError as e:
             app.logger.warning(f"Unsupported device found during discovery: {device.host} - {str(e)}")
         except Exception as e:
@@ -144,9 +144,9 @@ async def discover_devices(username=None, password=None, additional_broadcasts=N
             discovered_device = await Discover.discover_single(host=host, credentials=creds)
             await on_discovered(discovered_device)
             devices[host] = discovered_device
-            app.logger.debug(f"Discovered manual device: {host} with device type {discovered_device.device_type}")
+            app.logger.debug(f"Discovered device: {host}")
         except UnsupportedDeviceError as e:
-            app.logger.warning(f"Unsupported device found during manual discovery: {host} - {str(e)}")
+            app.logger.warning(f"Unsupported device found during discovery: {host} - {str(e)}")
         except Exception as e:
             app.logger.error(f"Error discovering device {host}: {str(e)}", exc_info=True)
 
