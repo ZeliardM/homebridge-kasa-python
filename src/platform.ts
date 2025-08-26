832d720ac5a43eaf9b9978c3dfaa57b9ce0c2733
ZeliardM <ZeliardM@gmail.com>
2025-08-26 06:45:09 -0400

    Fix Homebridge version check

--- src/platform.ts
+++ src/platform.ts
@@ -162,16 +162,16 @@ export default class KasaPythonPlatform implements DynamicPlatformPlugin {
       if (
         this.api.versionGreaterOrEqual &&
         !(
-          this.api.versionGreaterOrEqual('1.11') ||
-          this.api.versionGreaterOrEqual('2.0')
+          this.api.versionGreaterOrEqual('1.11.0') ||
+          this.api.versionGreaterOrEqual('2.0.0')
         )
       ) {
         throw new Error(
-          `homebridge-kasa-python requires Homebridge ^1.11 || ^2.0. Currently running: ${this.api.serverVersion}`,
+          `homebridge-kasa-python requires Homebridge ^1.11.0 || ^2.0. Currently running: ${this.api.serverVersion}`,
         );
       } else {
         this.log.debug(
-          `Homebridge version ${this.api.serverVersion} satisfies the requirement ^1.11 || ^2.0`,
+          `Homebridge version ${this.api.serverVersion} satisfies the requirement ^1.11.0 || ^2.0.0`,
         );
       }
     } catch (error) {
