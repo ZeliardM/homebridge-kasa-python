fe385cc3e5e80752e26e55718b36e00f8101031e
ZeliardM <ZeliardM@gmail.com>
2025-12-06 05:42:43 -0500

    [bug] Correct dependabot PR handling

--- .github/scripts/release_manager.py
+++ .github/scripts/release_manager.py
@@ -1305,12 +1305,12 @@ def _handle_pr_merge(context: Context) -> None:
         common.git_commit_files(
             [CHANGELOG_FILE],
             commit_message,
+            True,
         )
     else:
         common.git_commit_files(
             [CHANGELOG_FILE],
             commit_message,
-            True,
         )
     _ensure_repo_node_version(target_version, context=f"beta PR #{context.pull_request_number}")
     body = _build_beta_body(target_version, content)
